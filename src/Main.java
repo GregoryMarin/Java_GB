b0e825eebf6d2e3619be9cc407286f85e3eceed3
Gregory <gpost774@gmail.com>
2023-04-16 11:39:41 -0700

    First program

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,27 @@
+import java.time.LocalTime;
+import java.util.Scanner;
+
 // Press ⇧ twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press ⌥⏎ with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press ⌃R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press ⌃D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing ⌘F8.
-            System.out.println("i = " + i);
+        example();
+        }
+        static void example(){
+        System.out.println("What is your name: ");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        LocalTime time = LocalTime.now();
+        int hour = time.getHour();
+        if (hour >=5 && hour < 12) {
+            System.out.println("Good morning " + name + "!");
+        } else if (hour < 18) {
+            System.out.println("Good day " + name + "!");
+        } else if (hour < 23) {
+            System.out.println("Good evening " + name + "!");
+        } else {
+            System.out.println("Good night " + name + "!");
         }
     }
 }
+
