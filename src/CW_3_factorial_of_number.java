cd9befac68646fdc991827d2ba7dad3f539c9bbc
Gregory <gpost774@gmail.com>
2023-04-20 23:22:44 -0700

    Commited tasks

---
+++ src/CW_3_factorial_of_number.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class CW_3_factorial_of_number {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Write a number: ");
+        String f = sc.nextLine();
+        int n = Integer.parseInt(f);
+        var res = factorial(n);
+        System.out.println(res);
+    }
+    static double factorial(int n) {
+        if (n == 0) return 1;
+        if (n == 1) return 1;
+        return n * (factorial(n-1));
+    }
+}
