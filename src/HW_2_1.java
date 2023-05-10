481069f2f9148abafb03b84eea028415b5207f21
Gregory <gpost774@gmail.com>
2023-05-10 14:28:40 -0700

    Writed 2nd test

---
+++ src/HW_2_1.java
@@ -0,0 +1,29 @@
+import java.util.LinkedList;
+
+/*Реализуйте очередь с помощью LinkedList со следующими методами:
+enqueue() - помещает элемент в конец очереди,
+dequeue() - возвращает первый элемент из очереди и удаляет его,
+first() - возвращает первый элемент из очереди, не удаляя.*/
+public class HW_2_1 {
+    public static void main(String[] args) {
+        LinkedList<Integer> lst = HW_2_0.creatList(10,0,10);
+        System.out.println(lst);
+        enqueue(lst,99);
+        System.out.println(lst);
+        System.out.println(dequeue(lst));
+        System.out.println(lst);
+        System.out.println(first(lst));
+        System.out.println(lst);
+
+    }
+    static void enqueue(LinkedList<Integer> lst, int item){
+        lst.add(item);
+    }
+    static int dequeue(LinkedList<Integer> lst){
+        return lst.removeFirst();
+    }
+    static int first (LinkedList<Integer> lst){
+
+        return lst.getFirst();
+    }
+}
