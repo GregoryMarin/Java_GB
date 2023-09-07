062f6157a3dd9762204945ff29e90571ea88468d
Gregory <gpost774@gmail.com>
2023-09-06 20:04:59 -0700

    Commited some tasks

---
+++ src/CW_16.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class CW_16 {
+    public static void main(String[] args) {
+        int isLong = 109;
+        Scanner sc = new Scanner(System.in);
+        int h = sc.nextInt();
+        int head = h + 3;
+        double head2 = (double) head / 2;
+        System.out.println(head2);
+    }
+}
---
+++ src/sem_task2.java
@@ -0,0 +1,5 @@
+public class sem_task2 {
+    public static void main(String[] args) {
+
+    }
+}
